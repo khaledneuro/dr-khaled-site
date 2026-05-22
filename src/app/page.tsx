b42d3204f63dc1b1738c194266c7bc7dfe54b133
khaledneuro <khaledgad611@gmail.com>
2026-05-22 10:03:20 +0300

    email fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -399,15 +399,13 @@ export default function Home() {
   </a>{" "}
   (preferred)
 </p>
-
 <p className="mt-2">
   or{" "}
   <a
     href="mailto:khagad@moh.gov.kw"
     className="text-cyan-300 hover:underline"
-  >
-    khagad@moh.gov.kw
-    
+  >    khagad@moh.gov.kw
+   
   </a>
 </p>
         </div>
